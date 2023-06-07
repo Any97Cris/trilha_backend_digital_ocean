9494939bce05de0246bcbb3e055ee6418319c0c2
Any97Cris <criscianysilva1997@gmail.com>
2023-06-07 09:32:32 -0400

    array herois e endpoint read

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 
+const herois = ["mulher maravilha","capitã marvel","homem de ferro"]
+
 app.get('/', function (req, res) {
   res.send('Hello World')  
 })
 
 app.get('/oi', function (req, res) {
   res.send('Olá Mundo')  
+})
+
+app.get('/herois', function (req, res) {
+  res.send(herois)
 })
 
 app.listen(3000)
